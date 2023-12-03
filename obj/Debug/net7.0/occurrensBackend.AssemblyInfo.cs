cbf599a6f5116805ba21ca7b02d7f11e7e893cea
Jcobincho <jakubjucha800@gmail.com>
2023-12-03 22:25:21 +0100

    Create dto model for get all

--- obj/Debug/net7.0/occurrensBackend.AssemblyInfo.cs
+++ obj/Debug/net7.0/occurrensBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("occurrensBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f6e75eda0b93840926febcfd8a1a21d77a87d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("occurrensBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("occurrensBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
