136e0b9693f75f3a87e1aa9b87086642bd1403c6
Jcobincho <jakubjucha800@gmail.com>
2023-12-13 21:51:49 +0100

    Add disease validation

--- Models/DiseaseModels/AddDiseaseModelDto.cs
+++ Models/DiseaseModels/AddDiseaseModelDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace occurrensBackend.Models.DiseaseModels;
 
 public class AddDiseaseModelDto
 {
     public decimal Pesel { get; set; }
+
+
+    [Required(ErrorMessage = "Podaj nazwê chorody")]
     public string Name { get; set; }
+
+
+    [Required(ErrorMessage = "Podaj opis choroby")]
     public string Desctiption { get; set; }
+
+
+    [Required(ErrorMessage = "Podaj zalecenia dotycz¹ce leczenia!")]
     public string Medicines { get; set; }
 }
