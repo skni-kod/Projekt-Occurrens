de4490d94086265c2f68fd11730ffad57e1515b4
Jcobincho <jakubjucha800@gmail.com>
2023-12-16 01:46:49 +0100

    Create show single cart component

--- src/components/userNotLoggedIn/ShowAllDoctorsPage/PageComponents/DoctorCard.jsx
+++ src/components/userNotLoggedIn/ShowAllDoctorsPage/PageComponents/DoctorCard.jsx
@@ -1,8 +1,18 @@
 import classes from "./DoctorCard.module.css";
 
-export default function DoctorCard()
-{
-    return(
-        <div className={classes.show}>Karta</div>
-    );
+export default function DoctorCard() {
+  return (
+    <div className={classes.show}>
+      <div className={classes["flip-card"]}>
+        <div className={classes["flip-card-inner"]}>
+          <div className={classes["flip-card-front"]}>
+            <h1>Przód karty</h1>
+          </div>
+          <div class={classes["flip-card-back"]}>
+            <h1>Tył karty</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
