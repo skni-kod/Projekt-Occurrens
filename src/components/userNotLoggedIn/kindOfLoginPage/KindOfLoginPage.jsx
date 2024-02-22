c4f68d97a10563a0f12775ad44a08d945bc420e2
micang7 <mckinley@interia.pl>
2024-02-22 16:05:44 +0100

    Create a login or register role selection page

--- src/components/userNotLoggedIn/kindOfLoginPage/KindOfLoginPage.jsx
+++ src/components/userNotLoggedIn/kindOfLoginPage/KindOfLoginPage.jsx
@@ -1,6 +1,42 @@
+import classes from "./KindOfLoginPage.module.css";
+
 function KindOfLoginPage() {
-    return <h1>KindOfLoginPage</h1>;
-  }
-  
-  export default KindOfLoginPage;
-  
+  return (
+    <div className={classes.background}>
+      <div className={classes.container}>
+        <div className={`${classes.card} ${classes.doctor}`}>
+          <div className={classes.textContainer}>
+            <h3 className={classes.loginText}>
+              Zaloguj się jako
+              <span className={classes.loginRole}>lekarz</span>
+            </h3>
+            <button className={classes.loginBtn}>Zaloguj się</button>
+          </div>
+        </div>
+        <p className={classes.registerText}>
+          Nie masz konta? Zarejestruj się jako lekarz
+          <span className={classes.registerLink}>klikając tutaj</span>
+        </p>
+        <div className={classes.cardBackground}></div>
+      </div>
+      <div className={classes.container}>
+        <div className={`${classes.card} ${classes.patient}`}>
+          <div className={classes.textContainer}>
+            <h3 className={classes.loginText}>
+              Zaloguj się jako
+              <span className={classes.loginRole}>pacjent</span>
+            </h3>
+            <button className={classes.loginBtn}>Zaloguj się</button>
+          </div>
+        </div>
+        <p className={classes.registerText}>
+          Nie masz konta? Zarejestruj się jako pacjent
+          <span className={classes.registerLink}>klikając tutaj</span>
+        </p>
+        <div className={classes.cardBackground}></div>
+      </div>
+    </div>
+  );
+}
+
+export default KindOfLoginPage;
