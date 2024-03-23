204d2f36113ad25ddb2fde9127eed6b4d1f1a75d
Sebastian Zieliński <zielis277@wp.pl>
2024-03-23 23:17:18 +0100

    added calendar, chosen doctor and frame for chosing visits

--- src/components/userNotLoggedIn/visitSelection/visitSelection.jsx
+++ src/components/userNotLoggedIn/visitSelection/visitSelection.jsx
@@ -1,9 +1,149 @@
+import React, { useState } from 'react';
 import './visitSelection.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import BasicDoctorImage from "../../../images/BasicDoctorImage.png";
+
+
+
+function MyCalendar({minDate}) {
+    const [date, setDate] = useState(new Date());
+
+  const onChange = date => {
+    setDate(date);
+  }
+
+  return (
+    <div>
+      
+      <Calendar
+        locale="pl-PL"
+        onChange={onChange}
+        value={date}
+        minDate={minDate}
+        
+      />
+    </div>
+  );
+}
+
+function TextAreaComponent() {
+  const [textAreaInput, setTextAreaInput] = useState('');
+
+  const handleChange = (event) => {
+    setTextAreaInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <label>
+        <textarea 
+          value={textAreaInput} 
+          onChange={handleChange} 
+          placeholder="Wpisz wiadomość..." 
+          rows={8} 
+          cols={75} 
+        />
+      </label>
+      <p>Wprowadzona wiadomość: {textAreaInput}</p>
+    </div>
+  );
+}
+
+
+
+function FrameWithVisits(){
+  return(
+      <div  className='visitSection'>
+        <Visit></Visit>
+        <Visit></Visit>
+        <Visit></Visit>
+      </div>
+  )
+}
+
+function Visits(){
+  return (
+    <div className='visitsFrame'>
+      <div  className='visitHeading'>Wizyty</div>
+        <div>
+          <FrameWithVisits></FrameWithVisits>
+          <MessageToDoctor></MessageToDoctor>
+        </div>
+    </div>
+
+  )
+}
+
+function Visit(){
+  return<div className='visit'>
+    ALECHUJ (jest giga wielki)
+    <button className='wybierzGodzineButton'>
+      Wybierz godzinę
+    </button>
+  </div>
+}
+
+function ChosenDoctor(){
+  return(
+    <div className='chosenDoctor'>
+      <h1>Wybrany Doktor:</h1> 
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <DoctorInfo/>
+        <DoctorImage className='scaledImage'/>
+      </div>
+      
+    </div>
+  )
+}
+
+function DoctorInfo(){
+  return(
+    <div>
+      <h2>Imię:</h2>
+      <h2>Nazwisko:</h2>
+    </div>
+  )
+}
+
+function DoctorImage(){
+  return(
+    <img src={BasicDoctorImage}/>
+  )
+}
+
+function CalendarAndChosenDoctor(){
+  return(
+    <div className='calendarAndChosenDoctor'>
+        <MyCalendar className='react-calendar' 
+              minDate={new Date()}
+        />
+        <ChosenDoctor/>
+    </div>
+  )
+  }
+
+  function MessageToDoctor(){
+    return(
+      <div className='wybranyDoktor'>
+        Podaj wiadomość, którą chciałbyć dołączyć do zgłoszenia do lekarza (opis choroby, jak się czujesz, jakich leków potrzebujesz itd.):
+        <TextAreaComponent></TextAreaComponent>
+      </div>
+    )
+  }
+
+
+
 
 function VisitSelection() {
     return (
-    <div  className='background'>
+    <div className='background' style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <div className='mainframe'>
+        <CalendarAndChosenDoctor/>
+        <Visits/>
 
+
+      </div>
     </div>
     );
   }
