3ff032d03b27e1c836818f1929112ccbdc3f6143
Sebastian Zieliński <zielis277@wp.pl>
2024-04-03 15:44:24 +0200

    app.jsx returned to previous state

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import AboutPage from "./components/userNotLoggedIn/aboutPage/AboutPage";
 import ShowAllDoctorsPage from "./components/userNotLoggedIn/ShowAllDoctorsPage/ShowAllDoctorsPage";
 import KindOfLoginPage from "./components/userNotLoggedIn/kindOfLoginPage/KindOfLoginPage";
 
-import VisitSelection from "./components/userNotLoggedIn/visitSelection/visitSelection";
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -14,7 +12,7 @@ const router = createBrowserRouter([
     id: "main",
     children: [
       { index: true, element: <MainPage /> },
-      { path: "about", element: <VisitSelection /> },//bedzie chwilowo pozmieniane - tutaj ma być aboutPage!!
+      { path: "about", element: <AboutPage /> },
       { path: "doctors", element: <ShowAllDoctorsPage /> },
       { path: "login", element: <KindOfLoginPage /> },
     ],
