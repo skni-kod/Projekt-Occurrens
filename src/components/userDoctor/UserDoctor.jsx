3efe899c68c0dbf6a9ed979fe2f94a34db78bc7a
JMichu <michal0048@interia.pl>
2024-07-03 00:26:05 +0200

    data edit buttons changed to svg (still not interactive)

--- src/components/userDoctor/UserDoctor.jsx
+++ src/components/userDoctor/UserDoctor.jsx
@@ -1,4 +1,5 @@
 import styles from "./userDoctor.module.css";
+import editPencil from  "../../icons/edit-3-svgrepo-com.svg";
 
 function UserDoctor() {
     
@@ -9,7 +10,7 @@ function UserDoctor() {
         <div className={styles.transparent}>
             <h1>Twój profil</h1>
                 <div className={styles.doctorProfile}>
-                    <div className={`${styles.userData} ${styles.gridBox}`}><h2>Dane użytkownika</h2>
+                    <div className={`${styles.userData} ${styles.gridBox}`}><h2>Dane użytkownika <button><img src={editPencil}/></button></h2>
                         
                             <div className={styles.profileData}>
                                 <div>Imie:</div>                 <div>a  </div>
@@ -18,32 +19,34 @@ function UserDoctor() {
                                 <div>E-mail:</div>            <div> d </div>
                                 <div>Data urodzenia:</div>            <div> e </div>
                             </div>
-                        
+                            <div>
+                                
+                            </div>
                     </div>
-                    <div className={`${styles.specs} ${styles.gridBox}`}><h2>Specjalizacje</h2>
+                    <div className={`${styles.specs} ${styles.gridBox}`}><h2>Specjalizacje <button><img src={editPencil}/></button></h2>
                         <div className={styles.specsList}>
 
                         </div>
-                            <button className={styles.editDataButton}>Edytuj Dane</button>
-                            <button className={styles.editSpecsButton}>Edytuj Specjalizacje</button>
+                            
                     </div>
-                    <div className={`${styles.offices} ${styles.gridBox}`}><h2>Gabinety</h2>
+                    <div className={`${styles.offices} ${styles.gridBox}`}><h2>Gabinety <button><img src={editPencil}/></button></h2>
                     <div className={styles.officesData}>
-                                <div>Miasto:</div>                 <div>a  </div>
-                                <div>Ulica:</div>        <div> b </div>
+                                <div>Miasto:</div>                   <div> a </div>
+                                <div>Ulica:</div>                    <div> b </div>
                                 <div>Numer budynku:</div>            <div> c </div>
-                                <div>Numer lokalu:</div>            <div> d </div>
-                                <div>Kod pocztowy:</div>            <div> e </div>
-                                <h4 className={styles.open}>Godziny otwarcia:</h4>        <div></div>
+                                <div>Numer lokalu:</div>             <div> d </div>
+                                <div>Kod pocztowy:</div>             <div> e </div>
+                                <h4 className={styles.open}>
+                                Godziny otwarcia:</h4>              <div>  </div>
                                 <div>Poniedziałek:</div>            <div> e </div>
                                 <div>Wtorek:</div>                  <div> e </div>
-                                <div>Środa:</div>            <div> e </div>
-                                <div>Czwartek:</div>            <div> e </div>
-                                <div>Piątek:</div>            <div> e </div>
-                                <div>Sobota:</div>            <div> e </div>
-                                <div>Niedziela:</div>            <div> e </div>
+                                <div>Środa:</div>                   <div> e </div>
+                                <div>Czwartek:</div>                <div> e </div>
+                                <div>Piątek:</div>                  <div> e </div>
+                                <div>Sobota:</div>                  <div> e </div>
+                                <div>Niedziela:</div>               <div> e </div>
                             </div>
-                            <button className={styles.editOfficesButton}>Edytuj adresy i godziny</button>
+                            
                     </div>
                 </div>
         </div>
