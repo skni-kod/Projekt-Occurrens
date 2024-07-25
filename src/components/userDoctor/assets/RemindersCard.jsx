34fc3909c3754fccab1c13ae5e2894cd0292d4d2
Jakub Porzuczek <=>
2024-07-26 01:18:09 +0200

    Fix images, add reminders cards

--- src/components/userDoctor/assets/RemindersCard.jsx
+++ src/components/userDoctor/assets/RemindersCard.jsx
@@ -6,8 +6,10 @@ function RemindersCard(props) {
     return (
         <div className = {classes.card}>
             <img src={logo} className ={classes.logo}/>
-            <p className={classes.information}>{description}</p>
-            <button className = {[classes.button, classNameProps].join(' ')} onClick={method}><p className={classes.buttonTextStyle}>{buttonText}</p></button>
+            <div className={classes.components}>
+                <p className={classes.information}>{description}</p>
+                <button className = {[classes.button, classNameProps].join(' ')} onClick={method}><p className={classes.buttonTextStyle}>{buttonText}</p></button>
+            </div>
         </div>
     );
 }
--- src/components/userDoctor/startingPage/StartingPage.jsx
+++ src/components/userDoctor/startingPage/StartingPage.jsx
@@ -5,10 +5,16 @@ function StartingPage() {
       <>
         <div className={classes.background}>
           <div className={classes.header}>
-          <div className={classes.description}>
-          <p className={classes.information}>Witaj <br/>Tu zaczyna się Twoja podróż do <br/> efektywnej opieki  <br/> <span className={classes.blue}>pacjenta</span></p>
-          </div>
-          <img className={classes.doctor} src="https://i.imgur.com/ZvxPxuo.png" alt = "doctor"/>
+            <div className={classes.presentationContent}>
+              <div className={classes.description}>
+                <p className={classes.information}>Witaj <br/>Tu zaczyna się Twoja podróż do <br/> efektywnej opieki  <br/> <span className={classes.blue}>pacjenta</span></p>
+              </div>
+              <img className={classes.doctor} src="https://i.imgur.com/ZvxPxuo.png" alt = "doctor"/>
+            </div>
+            <div className={classes.reminders}>
+              <RemindersCard description = "Dzisiaj jest (data) (X) Pacjentów oczekuje na wizytę" buttonText = "Dzisiejsze wizyty"/>
+              <RemindersCard description = "Masz X nowych wizyt do przydzielenia" buttonText = "Wizyty do przedzielenia"/>
+            </div>
           </div>
           <br/>
         </div>
