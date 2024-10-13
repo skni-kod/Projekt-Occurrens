84b070f4bacd1e3a45db234f01b8f437f389ba08
Sebastian Zieliński <zielis277@wp.pl>
2024-10-14 00:01:50 +0200

    image update

---
+++ src/components/userPatient/afterVisit/AfterVisit.jsx
@@ -0,0 +1,155 @@
+import './AfterVisit.css';
+import doctorimage from './../../../../src/images/doctors.png';
+import paypal from './../../../../src/images/paypal.png';
+import visa from './../../../../src/images/visa.png';
+import mastercad from './../../../../src/images/mastercard.png';
+import americanexpress from './../../../../src/images/americane.png';
+
+function AfterVisit() {
+  return(
+  <div className='background'>
+    <MainFrame />
+  </div>)
+  }
+
+function MainFrame(){
+  return(
+    <div className='mainframe'>
+      <SummaryFrame />
+      <DoctorNote />
+    </div>
+  )  
+}
+
+function DoctorNote(){
+  return(
+    <div className='doctornote'>
+      <div className='headingnote'>Notatka od lekarza</div>
+    </div>
+  )
+}
+
+function SummaryFrame(){
+  return(
+    <div className='summaryframe'>
+      <SummaryHeading />
+      <SummaryAndImage />
+    </div>
+  )
+}
+
+function SummaryHeading(){
+  return(
+    <h1 className='summaryheading'>Dziękujemy za wizytę</h1>
+  )
+}
+
+function SummaryAndImage(){
+  return(
+    <div className='summaryandimage'>
+      <SummaryAndMedicines />
+      <ImageContainer />
+    </div>
+    
+  )
+}
+
+function SummaryAndMedicines(){
+  return(
+    <div className='summaryandmedicines'>
+      <Summary />
+      <Medicines />
+    </div>
+  )
+}
+
+function ImageContainer(){
+  return(
+    <div>
+      <img className='photo' src = {doctorimage} />
+    </div>
+  )
+}
+
+function Summary(){
+  return(
+    <div className='summary'>
+      <SmallHeading text={"Podsumowanie "} />
+      <VisitDetails text={"Data Wizyty "} information={"dipa"} />
+      <VisitDetails text={"Imie i Nazwisko Lekarza "} />
+      <VisitDetails text={"Choroba "} />
+      <VisitDetails text={"Do zapłaty "} /> 
+      <Payment />
+    </div>
+  )
+}
+
+function Medicines(){
+  return(
+    <div className='medicnes'>
+      <SmallHeading text={"Leki"} />
+      <MedicinesFieldAndSlider/>
+    </div>
+  )
+}
+
+function SmallHeading({text}){
+  return(
+    <div className='smallheading'>
+      {text}
+    </div>
+  )
+}
+
+function MedicinesFieldAndSlider({info}){
+  return(
+    <div className='medicinesfieldandslider'>
+      <MedicinesField text={info} />
+    </div>
+  )
+}
+
+function MedicinesField({text}){
+  return(
+    <div className='medicinesfield'>
+      {text}
+    </div>
+  )
+}
+
+function VisitDetails({text, information}){
+  return(
+    <div className='visitdetails'>
+      <TextDetails detail = {text} />
+      <VisitInfo info={information}/>
+    </div>
+  )
+}
+
+function TextDetails({detail}){
+  return(
+    <div className='detail'>
+      {detail}
+    </div>
+  )
+}
+function VisitInfo({info}){
+  return(
+    <div className='visitinfo'>
+      {info}
+    </div>
+  )
+}
+
+function Payment(){
+  return(
+    <div className='payment'>
+      dupa
+    </div>
+  )
+}
+
+
+  
+export default AfterVisit;
+  
